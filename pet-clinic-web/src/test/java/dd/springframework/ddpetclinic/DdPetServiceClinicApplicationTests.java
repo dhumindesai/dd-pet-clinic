7d9b829441dcbbd4365c25b851d5db3b65b9d596
dhumindesai <dhrumin.desai28@gmail.com>
2019-03-16 18:45:12 -0400

    Added Interfaces for Services. closes #5

--- pet-clinic-web/src/test/java/dd/springframework/ddpetclinic/DdPetServiceClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dd/springframework/ddpetclinic/DdPetServiceClinicApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DdPetClinicApplicationTests {
+public class DdPetServiceClinicApplicationTests {
 
     @Test
     public void contextLoads() {
