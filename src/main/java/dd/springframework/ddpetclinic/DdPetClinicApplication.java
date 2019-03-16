b8283dc89fc3c403a9039327d84ecd017e9f88b0
dhumindesai <dhrumin.desai28@gmail.com>
2019-03-16 11:36:05 -0400

    Initial commmit for maven spring project

---
+++ src/main/java/dd/springframework/ddpetclinic/DdPetClinicApplication.java
@@ -0,0 +1,13 @@
+package dd.springframework.ddpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dd/springframework/ddpetclinic/DdPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package dd.springframework.ddpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DdPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
