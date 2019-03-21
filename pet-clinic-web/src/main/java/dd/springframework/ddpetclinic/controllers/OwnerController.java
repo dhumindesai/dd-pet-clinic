7be136c1679861bb7ab667174aa60a21d380894f
dhumindesai <dhrumin.desai28@gmail.com>
2019-03-20 22:21:04 -0400

    Added Owner index page controller. closes #13

---
+++ pet-clinic-web/src/main/java/dd/springframework/ddpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package dd.springframework.ddpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping("")
+    public String listOwners(){
+        return "owners/index";
+    }
+}
